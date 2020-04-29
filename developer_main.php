0ea7f34e5dcb8db74c6fb849bb5489d30f658447
hamzapehlivan <hamza.pehlivan@ug.bilkent.edu.tr>
2020-04-29 19:18:40 +0300

    Initial Commit

---
+++ developer_main.php
@@ -0,0 +1,33 @@
+<html >
+	<head> 
+		<title> CSCareer</title>
+		<link rel="stylesheet" href="css/styles.css">
+	</head>
+	
+	<body>
+		
+		<header> 
+			<nav>
+				<ul>
+					<li> <a href = "developer_result.php">View Results</a> </li>
+					<li> <button>Profile</button></li>
+				</ul>
+				
+			</nav>
+			<h2>Take Quiz Now!</h2>
+		</header>
+		
+		<section> 
+			<div> <button>Generalist</button></div>
+			<div> <button>Front End</button></div>
+			<div> <button>Mobile</button></div>
+			<div> <button>Machine Learning</button></div>
+			<div> <button>DevOps</button></div>
+			<div> <button>Data Science</button></div>
+		</section>
+		
+		
+	</body>
+	
+
+</html>
---
+++ developer_result.php
@@ -0,0 +1,25 @@
+<html >
+	<head> 
+		<title> CSCareer</title>
+		<link rel="stylesheet" href="css/styles.css">
+	</head>
+	
+	<body>
+		
+		<header> 
+			<nav>
+				<ul>
+					<li> <a href = "developer_main.php">Main Menu</a> </li>
+					<li> <button>Profile</button></li>
+				</ul>
+				
+			</nav>
+			<h2>Results </h2>
+		</header>
+		
+		
+		
+	</body>
+	
+
+</html>
